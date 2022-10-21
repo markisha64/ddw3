3979410aa5c45d2a201041af34050ef95b756a52
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-21 21:29:50 +0100

    Added `dw3` building

---
+++ tools/generate_psx_iso_deps.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+
+import xml.etree.ElementTree as ET
+import argparse
+
+
+def main(args):
+	# Parse the xml file
+	tree = ET.parse(args.xml)
+	root = tree.getroot()
+	files = list(root.iter("file"))
+	files = map(lambda file: file.get("source"), files)
+	files = ' '.join(files)
+
+	with open(args.deps_file, "w") as deps_file:
+		deps_file.write(f"{args.iso}: {files}")
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--iso", dest="iso", type=str, required=True)
+	parser.add_argument("--xml", dest="xml", type=str, required=True)
+	parser.add_argument("--deps-file", dest="deps_file", type=str, required=True)
+
+	args = parser.parse_args()
+	main(args)
