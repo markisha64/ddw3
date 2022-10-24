2b48e58afcd8cf9207e3068e1cad3857c9e8657f
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-24 22:52:07 +0100

    All `S{addr}` labels are now local labels

--- dw2003/SLES_039.36/f0.s
+++ dw2003/SLES_039.36/f0.s
@@ -1,19 +1,19 @@
 # Loads the digimon stats
-S800134e8: addiu $sp, -24
-S800134ec: sw $ra, 16($sp)
-S800134f0: jal S800134ac
-S800134f4: nop
-S800134f8: move_ $v1, $v0
-S800134fc: bltz $v1, S80013524
-S80013500: move_ $v0, $zr
-S80013504: sll $v0, $v1, 0x1
-S80013508: addu $v0, $v1
-S8001350c: sll $v0, 0x2
-S80013510: subu $v0, $v1
-S80013514: sll $v0, 0x3
-S80013518: la_ $v1, S8003ef5c # Digimon stats
-S80013520: addu $v0, $v1
-S80013524: lw $ra, 16($sp)
-S80013528: nop
-S8001352c: jr $ra
-S80013530: addiu $sp, 24
+.L800134e8: addiu $sp, -24
+.L800134ec: sw $ra, 16($sp)
+.L800134f0: jal .L800134ac
+.L800134f4: nop
+.L800134f8: move_ $v1, $v0
+.L800134fc: bltz $v1, .L80013524
+.L80013500: move_ $v0, $zr
+.L80013504: sll $v0, $v1, 0x1
+.L80013508: addu $v0, $v1
+.L8001350c: sll $v0, 0x2
+.L80013510: subu $v0, $v1
+.L80013514: sll $v0, 0x3
+.L80013518: la_ $v1, .L8003ef5c # Digimon stats
+.L80013520: addu $v0, $v1
+.L80013524: lw $ra, 16($sp)
+.L80013528: nop
+.L8001352c: jr $ra
+.L80013530: addiu $sp, 24
