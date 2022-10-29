b33147e4b19e54e21d3a9894194f18cfbf6fd341
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-29 07:59:30 +0100

    Extracted `f2`.

---
+++ asm/dw2003/exe/f2.s
@@ -0,0 +1,31 @@
+.include "macros.s"
+
+.section "section_f2"
+.global f2
+f2:
+var_a=$s0
+var_b=$s1
+
+	addiu $sp, -32
+	sw $s0, 16($sp)
+	move_ $s0, $a0
+	sw $s1, 20($sp)
+	move_ $s1, $a1
+
+	# `0x80014504(&0x8001270c, 160, 172)`
+	la_ $a0, .L8001270c
+	li $a1, 160
+	sw $ra, 24($sp)
+	jal .L80014504
+	li $a2, 172
+
+	li $v1, 1
+	sw var_a, 80($v0)
+	sw $v1, 84($v0)
+	sw var_b, 88($v0)
+
+	lw $ra, 24($sp)
+	lw $s1, 20($sp)
+	lw $s0, 16($sp)
+	jr $ra
+	addiu $sp, 32
