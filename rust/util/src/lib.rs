0ec827dd457c121c10b00e2c64274973c2bd8ded
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-11-07 18:22:25 +0000

    Unused `panic_handler` function is now unsafe.

--- rust/util/src/lib.rs
+++ rust/util/src/lib.rs
@@ -16,7 +16,7 @@ pub macro decl_static($section:literal, $($vis:vis static mut $NAME:ident: $T:ty
 #[no_mangle]
 #[panic_handler]
 #[inline(never)]
-pub fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+pub unsafe fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
 	// SAFETY: This function will not make it to the final binary
 	//         due to our linker script discarding it and being
 	//         annotated with `inline(never)`
