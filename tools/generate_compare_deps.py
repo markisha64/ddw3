2a62611ce07c791bc4514b491887620e6d44de3f
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-07-10 11:10:40 +0100

    `generate_compare_deps.py` now has a command-line interface.

--- tools/generate_compare_deps.py
+++ tools/generate_compare_deps.py
@@ -1,11 +1,28 @@
 #!/bin/env python3
 
-deps = []
-with open("checksums.sha256") as f:
-	for line in f:
-		[_, dep_path] = line.split('  ')
-		dep_path = dep_path.rstrip('\n')
-		deps.append(dep_path)
+import argparse
 
-deps = ' '.join(deps)
-print(f"build/compare.ok: {deps}")
+
+def main(args):
+	# Build the dependencies
+	deps = []
+	with open(args.checksums) as f:
+		for line in f:
+			[_, dep_path] = line.split('  ')
+			dep_path = dep_path.rstrip('\n')
+			deps.append(dep_path)
+	deps = ' '.join(deps)
+
+	# Then write them
+	with open(args.deps_file, "w") as deps_file:
+		deps_file.write(f"{args.compare_output}: {deps}")
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--checksums", dest="checksums", type=str, required=True)
+	parser.add_argument("--compare-output", dest="compare_output", type=str, required=True)
+	parser.add_argument("--deps-file", dest="deps_file", type=str, required=True)
+
+	args = parser.parse_args()
+	main(args)
