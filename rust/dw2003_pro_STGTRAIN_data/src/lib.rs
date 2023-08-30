bc5c2438aba2092488e1417b5f8a1ece83646b44
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-08-30 16:42:06 +0100

    Extracted `STGTRAIN_data`

---
+++ rust/dw2003_pro_STGTRAIN_data/src/lib.rs
@@ -0,0 +1,13 @@
+//! `ddw3` `STGTRAIN.PRO` data
+
+// Features
+#![feature(decl_macro)]
+#![no_std]
+// Lints
+#![allow(non_snake_case)]
+
+// Modules
+mod data;
+
+// Export
+pub use util::panic_handler;
