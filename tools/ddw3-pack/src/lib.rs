cbdb6a5db9482550b98e87cbba4a7063cf1f0a28
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-12-01 22:52:50 +0000

    Extracted all entries in `dw2003/AAA/DAT/FIELD/STAGE` Updated `unpack` and `mkpack` to allow null entries.

--- tools/ddw3-pack/src/lib.rs
+++ tools/ddw3-pack/src/lib.rs
@@ -1,22 +1,20 @@
 //! `PACK` files
 
 // Features
-#![feature(seek_stream_len, lint_reasons)]
-
-use byteorder::WriteBytesExt;
+#![feature(seek_stream_len, lint_reasons, array_windows, stmt_expr_attributes)]
 
 // Imports
 use {
 	anyhow::Context,
-	byteorder::{LittleEndian, ReadBytesExt},
+	byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt},
 	ddw3_util::IoSlice,
 	std::io,
 };
 
 /// Pack file reader
 pub struct PackReader<R> {
-	/// File boundaries
-	boundaries: Vec<u32>,
+	/// File slices
+	slices: Vec<(u32, u32)>,
 
 	/// Inner reader
 	reader: R,
@@ -28,40 +26,56 @@ impl<R> PackReader<R> {
 	where
 		R: io::Read + io::Seek,
 	{
-		// Read the first boundary to get the size of the header
-		let first_boundary = self::read_boundary(&mut reader)?;
-		let boundaries_len = first_boundary / 4;
-		let boundaries_len = boundaries_len
-			.try_into()
-			.context("Number of boundaries didn't fit into `usize`")?;
-
-		// Then read the rest of the boundaries
-		let mut boundaries = Vec::with_capacity(boundaries_len);
-		boundaries.push(first_boundary);
-		for idx in 1..boundaries_len {
-			// Note: Guaranteed to not panic, since we start at boundary 1
-			let last_boundary = boundaries[idx - 1];
-
-			let boundary = self::read_boundary(&mut reader)?;
-			anyhow::ensure!(boundary > last_boundary, "Expected boundaries to be in ascending order");
-
-			boundaries.push(boundary);
+		// Read the first non-null offset to get the size of the header
+		let mut null_starting_offsets = 0;
+		let mut first_non_null_offset = self::read_offset(&mut reader)?;
+		while first_non_null_offset == 0 {
+			null_starting_offsets += 1;
+			first_non_null_offset = self::read_offset(&mut reader)?
 		}
-		let reader_len = reader
-			.stream_len()
-			.context("Unable to get stream length")?
+		let offsets_len = first_non_null_offset / 4;
+		let offsets_len = offsets_len
 			.try_into()
-			.context("Stream length didn't fit into a `u32`")?;
-		boundaries.push(reader_len);
+			.context("Number of offsets didn't fit into `usize`")?;
 
-		Ok(Self { boundaries, reader })
+		// Then read the rest of the offsets
+		let mut offsets = (null_starting_offsets + 1..offsets_len)
+			.map(|_| self::read_offset(&mut reader))
+			.collect::<Result<Vec<_>, anyhow::Error>>()
+			.context("Unable to read all offsets")?;
+		offsets.splice(0..0, (0..null_starting_offsets).map(|_| 0));
+		offsets.insert(null_starting_offsets, first_non_null_offset);
+
+		// And finally get all file slices
+		let reader_size = reader.seek(io::SeekFrom::End(0)).context("Unable to get reader size")?;
+		let reader_size = reader_size.try_into().context("Reader size didn't fit into a `u32`")?;
+		let slices = offsets
+			.iter()
+			.enumerate()
+			.map(|(offset_idx, &start)| {
+				if start == 0 {
+					return (0, 0);
+				}
+
+				// Find the end of this file, skipping any empty files
+				let end = offsets[offset_idx + 1..]
+					.iter()
+					.find(|&&offset| offset != 0)
+					.copied()
+					.unwrap_or(reader_size);
+
+				(start, end)
+			})
+			.collect();
+
+		Ok(Self { slices, reader })
 	}
 
 	/// Returns the number of files
 	#[expect(clippy::len_without_is_empty, reason = "We're never empty")]
 	pub fn len(&self) -> usize {
-		// Note: We always have at least 2 boundaries
-		self.boundaries.len() - 1
+		// Note: We always have at least 1 slice
+		self.slices.len()
 	}
 
 	/// Reads the `n`th file
@@ -69,10 +83,10 @@ impl<R> PackReader<R> {
 	where
 		R: io::Seek,
 	{
-		let start = self.boundaries.get(idx).context("Invalid index")?;
-		let start = u64::from(*start);
-		let end = self.boundaries.get(idx + 1).context("Invalid index")?;
-		let end = u64::from(*end);
+		let &(start, end) = self.slices.get(idx).context("Invalid index")?;
+		let start = u64::from(start);
+		let end = u64::from(end);
+
 		IoSlice::new(&mut self.reader, start..end).context("Unable to create io slice")
 	}
 }
@@ -83,59 +97,79 @@ where
 	I: IntoIterator<Item = RF>,
 	I::IntoIter: ExactSizeIterator,
 	RF: FnOnce() -> Result<R, anyhow::Error>,
-	R: io::Read,
+	R: io::Read + io::Seek,
 	W: io::Write + io::Seek,
 {
 	// Skip past the header for now
 	let files = files.into_iter();
-	let boundaries_len = u32::try_from(files.len()).context("Number of files didn't fit into a `u32`")?;
-	let header_size = 4 * u64::from(boundaries_len);
+	let offsets_len = u32::try_from(files.len()).context("Number of files didn't fit into a `u32`")?;
+	let header_size = 4 * offsets_len;
 	writer
-		.seek(io::SeekFrom::Start(header_size))
+		.seek(io::SeekFrom::Start(u64::from(header_size)))
 		.context("Unable to seek writer past header")?;
 
-	// Then write each file
-	let mut cur_pos = header_size;
-	let mut boundaries = Vec::with_capacity(files.len());
+	// Then write all files
+	let mut cur_pos = u64::from(header_size);
+	let mut offsets = Vec::with_capacity(files.len());
 	for file_creator in files {
+		// Open the file to write
 		let mut file = file_creator().context("Unable to create reader")?;
+		let file_len = file.stream_len().context("Unable to get reader length")?;
 
-		// Push our current position as a boundary
-		let boundary = u32::try_from(cur_pos).context("Boundary didn't fit into a `u32`")?;
-		boundaries.push(boundary);
+		// If the file is empty, push a 0 to the offsets instead
+		if file_len == 0 {
+			offsets.push(0);
+			continue;
+		}
 
-		// Then write the file
-		let file_len = io::copy(&mut file, writer).context("Unable to copy file to writer")?;
-		cur_pos += file_len;
-
-		// And pad the entry to word size
-		if cur_pos % 4 != 0 {
-			let remaining = 4 - (cur_pos % 4);
+		// Align ourselves to word size if the file isn't empty
+		if file_len != 0 && cur_pos % 4 != 0 {
+			let remaining = cur_pos.next_multiple_of(4) - cur_pos;
 			writer
 				.write_all(&[0u8; 4][..remaining as usize])
 				.context("Unable to pad output to word size")?;
 			cur_pos += remaining;
 		}
+
+		// Write the start offset
+		let offset = u32::try_from(cur_pos).context("offset didn't fit into a `u32`")?;
+		offsets.push(offset);
+
+		// Then write the file
+		io::copy(&mut file, writer).context("Unable to copy file to writer")?;
+		cur_pos += file_len;
+	}
+
+	// Finally pad, if there were no empty files
+	// TODO: Why is it like this?
+	if offsets.iter().all(|&offset| offset != 0) && cur_pos % 4 != 0 {
+		let remaining = 4 - (cur_pos % 4);
+		writer
+			.write_all(&[0u8; 4][..remaining as usize])
+			.context("Unable to pad output to word size")?;
+
+		#[expect(unused_assignments, reason = "It might be used in the future")]
+		cur_pos += remaining;
 	}
 
 	// Finally go back and write the header
 	writer.rewind().context("Unable to rewind writer")?;
-	for boundary in boundaries {
+	for offset in offsets {
 		writer
-			.write_u32::<LittleEndian>(boundary)
-			.context("Unable to write boundary")?;
+			.write_u32::<LittleEndian>(offset)
+			.context("Unable to write offset")?;
 	}
 
 	Ok(())
 }
 
-/// Reads a boundary from `R`
-fn read_boundary<R>(reader: &mut R) -> Result<u32, anyhow::Error>
+/// Reads a offset from `R`
+fn read_offset<R>(reader: &mut R) -> Result<u32, anyhow::Error>
 where
 	R: io::Read,
 {
-	let boundary = reader.read_u32::<LittleEndian>().context("Unable to read boundary")?;
-	anyhow::ensure!(boundary % 4 == 0, "Expected boundary to be aligned to word size");
+	let offset = reader.read_u32::<LittleEndian>().context("Unable to read offset")?;
+	anyhow::ensure!(offset % 4 == 0, "Expected offset to be aligned to word size");
 
-	Ok(boundary)
+	Ok(offset)
 }
