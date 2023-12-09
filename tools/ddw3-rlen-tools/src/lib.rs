e27901bc936e84011e703592fb015174a4240cc1
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-12-09 22:57:13 +0000

    Renamed all map files to lowercase.

--- tools/ddw3-rlen-tools/src/lib.rs
+++ tools/ddw3-rlen-tools/src/lib.rs
@@ -13,7 +13,7 @@ pub struct Config {
 	/// Run in compatibility mode for file.
 	///
 	/// Expects a string of format `<map>.<tile>`,
-	/// for example, `S232.12`
+	/// for example, `s232.12`
 	#[serde(default, skip_serializing_if = "Option::is_none")]
 	pub compatibility: Option<String>,
 }
