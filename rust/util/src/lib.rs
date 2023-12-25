c3d9fe618b0a1fc272ac0ec4e79dbfcc32c780b9
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-12-25 19:39:38 +0000

    `decl_static` macro now allows doc comments on each item.

--- rust/util/src/lib.rs
+++ rust/util/src/lib.rs
@@ -4,10 +4,16 @@
 #![feature(decl_macro)]
 #![no_std]
 
-pub macro decl_static($section:literal, $($vis:vis static mut $NAME:ident: $T:ty = $value:expr; )*) {
+pub macro decl_static($section:literal,
+	$(
+		$( #[$meta:meta] )*
+		$vis:vis static mut $NAME:ident: $T:ty = $value:expr;
+	)*
+) {
 	$(
 		#[no_mangle]
 		#[link_section = $section]
+		$( #[$meta] )*
 		$vis static mut $NAME: $T = $value;
 	)*
 }
