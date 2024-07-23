54741235054dbcead7a3e918563e2d629e5b6ad0
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-07-23 18:14:58 +0100

    Improved implementation of dummy panic handler.

--- rust/util/src/lib.rs
+++ rust/util/src/lib.rs
@@ -19,14 +19,12 @@ pub macro decl_static($section:literal,
 }
 
 // Dummy panic handler
+// Note: This function will not make it to the final binary
+//       due to our linker script discarding it and being
+//       annotated with `inline(never)`
 #[no_mangle]
 #[panic_handler]
 #[inline(never)]
-pub unsafe fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-	// SAFETY: This function will not make it to the final binary
-	//         due to our linker script discarding it and being
-	//         annotated with `inline(never)`
-	unsafe {
-		core::hint::unreachable_unchecked();
-	}
+pub fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+	loop {}
 }
