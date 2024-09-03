212fd99b65d7a0d9cb2ac321c0b9778d9eab57d7
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-09-03 19:24:14 +0100

    `unpack` now creates a better named output file.

--- tools/unpack.sh
+++ tools/unpack.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-OUTPUT=$(printf "$1" | sed -e 's/.bin$//')
+OUTPUT=$(printf "$1" | sed -Ee 's/(.*?)\.(bin|BIN)$/\L\1/')
 
+mkdir -p "$OUTPUT"
 cargo run --manifest-path=tools/Cargo.toml --bin unpack -- \
 	"$1" \
 	--output "$OUTPUT/" \
